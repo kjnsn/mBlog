79807a056f95bddc75b7a875bd75d2f7dbcdacf8
kjnsn <thekjnsn@gmail.com>
2016-12-21 20:32:30 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-# mBlog v0.4.0 - Copyright 2012 Lachlan Main <lachlan.main@gmail.com>
+# mBlog v0.4.0 - Copyright 2012 Kaley Main <kaleypoole17@gmail.com>
 # Licensed under the GPL version 3 <http://www.gnu.org/licenses/gpl-3.0.html>
 
 require_once 'markdown.php';
